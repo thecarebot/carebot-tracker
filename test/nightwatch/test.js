4ed27ba072690a113a5800a7143edb108322115e
Matt Hampel <matth@localdata.com>
2016-02-03 16:09:15 -0500

    Another stab at nightwatch

---
+++ test/nightwatch/test.js
@@ -0,0 +1,40 @@
+/*jslint node: true */
+/*globals describe, it */
+'use strict';
+
+var nightwatch = require('nightwatch');
+
+describe('Google demo test for Mocha', function() {
+
+  describe('with Nightwatch', function() {
+
+    before(function(client, done) {
+      done();
+    });
+
+    after(function(client, done) {
+      client.end(function() {
+        done();
+      });
+    });
+
+    afterEach(function(client, done) {
+      done();
+    });
+
+    beforeEach(function(client, done) {
+      done();
+    });
+
+    it('uses BDD to run the Google simple test', function(client) {
+      client
+        .url('http://google.com')
+        .expect.element('body').to.be.present.before(1000);
+
+      client.setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
+        .pause(1000)
+        .assert.containsText('#main', 'Night Watch');
+    });
+  });
+});
+
--- test/test.js
+++ test/test.js
@@ -2,58 +2,11 @@
 /*globals describe, it */
 'use strict';
 
-var nightwatch = require('nightwatch');
+var mocha = require('mocha');
+var should = require('should');
 
-describe('Github', function() {
-  var client = nightwatch.initClient({
-    silent : true
+describe('VisibilityTracker', function () {
+  describe('startTimer', function() {
+    it('should work');
   });
-
-  var browser = client.api();
-
-  this.timeout(99999999);
-
-  before(function() {
-
-    browser.perform(function() {
-      console.log('beforeAll')
-    });
-
-  });
-
-  beforeEach(function(done) {
-    browser.perform(function() {
-      console.log('beforeEach')
-    });
-
-    client.start(done);
-  });
-
-
-  it('Demo test GitHub', function (done) {
-    browser
-      .url('https://github.com/nightwatchjs/nightwatch')
-      .waitForElementVisible('body', 5000)
-      .assert.title('nightwatchjs/nightwatch · GitHub')
-      .waitForElementVisible('body', 1000)
-      .assert.visible('.container .breadcrumb a span')
-      .assert.containsText('.container .breadcrumb a span', 'nightwatch', 'Checking project title is set to nightwatch');
-
-    client.start(done);
-  });
-
-  afterEach(function() {
-    browser.perform(function() {
-      console.log('afterEach')
-    });
-  });
-
-  after(function(done) {
-    browser.end(function() {
-      console.log('afterAll')
-    });
-
-    client.start(done);
-  });
-
 });
