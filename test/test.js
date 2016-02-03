859031c6eebb5e83f355cbe50782b834544c2fc3
Matt Hampel <matth@localdata.com>
2016-02-03 15:06:13 -0500

    Add nightwatch

--- test/test.js
+++ test/test.js
@@ -2,11 +2,58 @@
 /*globals describe, it */
 'use strict';
 
-var mocha = require('mocha');
-var should = require('should');
+var nightwatch = require('nightwatch');
 
-describe('VisibilityTracker', function () {
-  describe('startTimer', function() {
-    it('should work');
+describe('Github', function() {
+  var client = nightwatch.initClient({
+    silent : true
   });
+
+  var browser = client.api();
+
+  this.timeout(99999999);
+
+  before(function() {
+
+    browser.perform(function() {
+      console.log('beforeAll')
+    });
+
+  });
+
+  beforeEach(function(done) {
+    browser.perform(function() {
+      console.log('beforeEach')
+    });
+
+    client.start(done);
+  });
+
+
+  it('Demo test GitHub', function (done) {
+    browser
+      .url('https://github.com/nightwatchjs/nightwatch')
+      .waitForElementVisible('body', 5000)
+      .assert.title('nightwatchjs/nightwatch · GitHub')
+      .waitForElementVisible('body', 1000)
+      .assert.visible('.container .breadcrumb a span')
+      .assert.containsText('.container .breadcrumb a span', 'nightwatch', 'Checking project title is set to nightwatch');
+
+    client.start(done);
+  });
+
+  afterEach(function() {
+    browser.perform(function() {
+      console.log('afterEach')
+    });
+  });
+
+  after(function(done) {
+    browser.end(function() {
+      console.log('afterAll')
+    });
+
+    client.start(done);
+  });
+
 });
