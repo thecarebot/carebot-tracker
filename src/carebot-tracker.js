d8f4f50f59fcf87c3a24097cbeac59fff8bc6ad3
Matt Hampel <matth@localdata.com>
2016-02-04 11:35:20 -0500

    Better tracker, fixes #1, fixes #7, fixes #8

--- src/carebot-tracker.js
+++ src/carebot-tracker.js
@@ -21,28 +21,39 @@
     lib.Timer = function() {
         // From https://github.com/nprapps/elections16/blob/master/www/js/app.js#L298-L335
         var startTime;
+        var previousTotalSeconds = 0;
 
         function getTimeBucket(seconds) {
+            var minutes, timeBucket;
             if (seconds < 60) {
                 var tensOfSeconds = Math.floor(seconds / 10) * 10;
-                var timeBucket = tensOfSeconds.toString() + 's';
+                timeBucket = tensOfSeconds.toString() + 's';
             } else if (seconds >=60 && seconds < 300) {
-                var minutes = Math.floor(seconds / 60);
-                var timeBucket = minutes.toString() + 'm';
+                minutes = Math.floor(seconds / 60);
+                timeBucket = minutes.toString() + 'm';
             } else {
-                var minutes = Math.floor(seconds / 60);
+                minutes = Math.floor(seconds / 60);
                 var fivesOfMinutes = Math.floor(minutes / 5) * 5;
-                var timeBucket = fivesOfMinutes.toString() + 'm';
+                timeBucket = fivesOfMinutes.toString() + 'm';
             }
 
             return timeBucket;
-        };
+        }
 
-        function calculateTimeBucket(startTime) {
+        function getSecondsSince(startTime) {
+            if (!startTime) {
+                return 0;
+            }
+
             var currentTime = new Date();
             var totalTime = Math.abs(currentTime - startTime);
             var seconds = Math.floor(totalTime/1000);
-            var timeBucket = this.getTimeBucket(seconds);
+            return seconds;
+        }
+
+        function calculateTimeBucket(startTime) {
+            var totalTime = getSecondsSince(startTime) + previousTotalSeconds;
+            var timeBucket = getTimeBucket(totalTime);
 
             return [timeBucket, totalTime];
         }
@@ -53,6 +64,18 @@
 
         function start() {
             startTime = new Date();
+        }
+
+        function pause() {
+            previousTotalSeconds = getSecondsSince(startTime) + previousTotalSeconds;
+            startTime = undefined;
+        }
+
+        return {
+            start: start,
+            pause: pause,
+            check: check,
+            getTimeBucket: getTimeBucket
         };
     }.bind(this);
 
@@ -65,43 +88,36 @@
      * @param {Object} config Configuration to override the default settings.
      */
     lib.VisibilityTracker = function(id, callback, config) {
-        this.MS_WAIT = 50;
+        var WAIT_TO_ENSURE_SCROLLING_IS_DONE = 50; //
 
-        this.id = id;
-        this.el = document.getElementById(id);
-        this.timeout = undefined;
-        this.isVisible = false;
+        var el = document.getElementById(id);
+        var isVisible = false;
+        var timeout;
 
-        this.timer = new lib.Timer();
+        var timer = new lib.Timer();
 
         // Ensure a config object
         config = (config || {});
 
 
-        this.onBucket = function() {
-            // TODO
+        function onBucket() {
             callback(this.lastBucket);
-        }.bind(this);
+        }
 
-        this.startTimer = function() {
-            // TODO
-        };
-
-        this.stopTimer = function() {
-            // TODO
-        };
-
-        this.isElementInViewport = function(el) {
-            // Via http://stackoverflow.com/a/15203639/117014
-            var rect     = el.getBoundingClientRect(),
-                vWidth   = window.innerWidth || doc.documentElement.clientWidth,
-                vHeight  = window.innerHeight || doc.documentElement.clientHeight,
-                efp      = function (x, y) { return document.elementFromPoint(x, y) };
+        function isElementInViewport(el) {
+            // Adapted from http://stackoverflow.com/a/15203639/117014
+            var rect     = el.getBoundingClientRect();
+            var vWidth   = window.innerWidth || document.documentElement.clientWidth;
+            var vHeight  = window.innerHeight || document.documentElement.clientHeight;
+            var efp      = function(x, y) {
+                return document.elementFromPoint(x, y);
+            };
 
             // Return false if it's not in the viewport
             if (rect.right < 0 || rect.bottom < 0
-                    || rect.left > vWidth || rect.top > vHeight)
+                    || rect.left > vWidth || rect.top > vHeight) {
                 return false;
+            }
 
             // Return true if any of its four corners are visible
             return (
@@ -110,61 +126,58 @@
               ||  el.contains(efp(rect.right, rect.bottom))
               ||  el.contains(efp(rect.left,  rect.bottom))
             );
-        };
+        }
 
-        this.checkIfVisible = function() {
-            var newVisibility = this.isElementInViewport(this.el);
-            console.log(this.isVisible, newVisibility);
+        function checkIfVisible () {
+            var newVisibility = isElementInViewport(el);
+            console.log(isVisible, newVisibility);
 
-            if (this.isVisible && !newVisibility) {
+            if (isVisible && !newVisibility) {
                 console.log("Became hidden");
-                console.log(this.timer.check());
+                timer.pause();
+                console.log(timer.check());
                 // The item has become invisible
                 // TODO
                 // Start the timer
             }
 
-            if (!this.isVisible && newVisibility) {
+            if (!isVisible && newVisibility) {
                 console.log("Became visible");
                 console.log(this.timer);
-                this.timer.start();
+                timer.start();
                 // The item has become visible
                 // Stop the timer
             }
 
-            this.isVisible = newVisibility;
+            isVisible = newVisibility;
             return newVisibility;
-        }.bind(this);
+        }
 
-        this.handler = function() {
+        function handler() {
             // Only register a new event every 1/10 of a second
             // That way we don't record an absurd number of events
-            if (this.timeout) {
-                window.clearTimeout(this.timeout);
+            if (timeout) {
+                window.clearTimeout(timeout);
             }
-            this.timeout = window.setTimeout(this.checkIfVisible, this.MS_WAIT);
-        }.bind(this);
+            timeout = window.setTimeout(checkIfVisible, WAIT_TO_ENSURE_SCROLLING_IS_DONE);
+        }
 
         // Listen to different window movement events
         if (window.addEventListener) {
-            addEventListener('DOMContentLoaded', this.handler, false);
-            addEventListener('load', this.handler, false);
-            addEventListener('scroll', this.handler, false);
-            addEventListener('resize', this.handler, false);
+            addEventListener('DOMContentLoaded', handler, false);
+            addEventListener('load', handler, false);
+            addEventListener('scroll', handler, false);
+            addEventListener('resize', handler, false);
         } else if (window.attachEvent)  {
-            attachEvent('onDOMContentLoaded', this.handler); // IE9+ :(
-            attachEvent('onload', this.handler);
-            attachEvent('onscroll', this.handler);
-            attachEvent('onresize', this.handler);
+            attachEvent('onDOMContentLoaded', handler); // IE9+ :(
+            attachEvent('onload', handler);
+            attachEvent('onscroll', handler);
+            attachEvent('onresize', handler);
         }
 
-        // Add any overrides to settings coming from config.
-        var key;
-        for (key in config) {
-            this.settings[key] = config[key];
-        }
-        return this;
-    }.bind(this);
+        // return {
+        // };
+    };
 
     return lib;
 });
