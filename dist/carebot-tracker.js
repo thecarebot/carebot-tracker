c99f6da40de642f401a2832d52cb29d231ef0ec2
Matt Hampel <matth@localdata.com>
2016-02-10 16:11:44 -0500

    Update dist

--- dist/carebot-tracker.js
+++ dist/carebot-tracker.js
@@ -1,4 +1,4 @@
-/*! carebot-tracker - v0.1.0 - 2016-02-08 */
+/*! carebot-tracker - v0.1.0 - 2016-02-10 */
 /*
 * carebot-tracker.js is library that checks if an element is visible on the page
 * and reports it to pym.js.
