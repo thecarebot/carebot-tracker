51e152c995a4c52a5e43643c946de22970bc5bc3
Matt Hampel <matth@localdata.com>
2016-03-02 15:43:13 -0500

    Add scroll depth tracker

--- README.md
+++ README.md
@@ -8,9 +8,8 @@ A tool for tracking analytics that matter.
 
 ### Visibiltiy Tracker
 
-This is the main tool in the Carebot Tracker right now. It records how long a
-viz has been on screen. After including `carebot-tracker.js` on a page,
-here's how you initailize the tracker:
+The Visibility Tracker records how long an element has been on screen. After
+including `carebot-tracker.js` on a page, here's how you initailize the tracker:
 
 ```
 var tracker = new CarebotTracker.VisibilityTracker('element-id', function(bucket) {
@@ -18,82 +17,14 @@ var tracker = new CarebotTracker.VisibilityTracker('element-id', function(bucket
 });
 ```
 
+
+### ScrollTracker
+
 ### Timer
 
-The timer is a utility class that works like a stopwatch.
+The timer is a utility class .
 
-#### Time buckets
 
-The timer's special feature is that it returns times in the
-standard NPR time buckets as strings (in addition to a plain `seconds` count).
-
-The time buckets are:
-* From zero up to 59 seconds: 10 second intervals (eg `10s`, `20s`, `30s`...)
-* 60 up to 300 seconds: one-minute intervals (eg `1m`, `2m`...)
-* More than 300 seconds: five-minute intervals (eg `5m`, `10m`...)
-
-#### Methods
-
-##### Constructor
-
-```
-var timer = new CarebotTracker.Timer();
-```
-
-An optional callback will be called on every new bucket:
-
-```
-var timer = new CarebotTracker.Timer(function(result) {
-  console.log(result.bucket, result.seconds);
-});
-```
-
-##### `start`
-
-Starts the timer.
-
-```
-var timer = new CarebotTracker.Timer();
-timer.start();
-```
-
-##### `pause`
-
-Pauses the timer. Note that this does not zero out the timer value.
-
-```
-var timer = new CarebotTracker.Timer();
-timer.start();
-timer.pause();
-```
-
-##### `check`
-Gets the seconds elapsed and current time bucket
-
-```
-var timer = new CarebotTracker.Timer();
-timer.start();
-// wait 300 seconds
-console.log(timer.check());
-// prints { bucket: '5m', seconds: 300 }
-```
-
-#### Example
-
-```
-var timer = new CarebotTracker.Timer();
-timer.start();
-// wait 300 seconds
-timer.pause();
-
-console.log(timer.check());
-// prints { bucket: '5m', seconds: 300 }
-
-timer.start();
-// wait 60 seconds
-timer.check();
-// prints { bucket: '5m', seconds: 360 }
-```
 
 ## Development
 
--- src/carebot-tracker.js
+++ src/carebot-tracker.js
@@ -29,7 +29,7 @@
         // From https://github.com/nprapps/elections16/blob/master/www/js/app.js#L298-L335
         var totalSeconds = 0;
         var previousBucket;
-c
+
         function getTimeBucket(seconds) {
             var minutes, timeBucket;
             if (seconds < 60) {
@@ -83,23 +83,118 @@ c
         });
 
         $.screentime({
-          fields: [
-            { selector: '#' + id,
-              name: 'graphic'
+            fields: [
+                { selector: '#' + id,
+                    name: 'graphic'
+                }
+            ],
+            callback: function(data) {
+                timer.add(data.graphic);
             }
-          ],
-          callback: function(data) {
-            timer.add(data.graphic);
-          }
         });
     };
+
 
     /**
      * Tracks scroll depth
      */
-    lib.ScrollTracker = function(id) {
-        id = id || '#'
-    }
+    lib.ScrollTracker = function(id, callback, config) {
+        console.log("Setting up scrollTracker", config);
+        var elt = document.getElementById(id);
+        var previousBucket = 0;
+
+        function getPageScroll() {
+            var body = document.body;
+            var docEl = document.documentElement;
+
+
+            var scrollTop;
+            var scrollLeft;
+
+            if (window.hasOwnProperty('pageYOffset')) {
+                scrollTop = window.pageYOffset;
+                scrollLeft = window.pageXOffset;
+            } else if (docEl.hasOwnProperty('scrollTop')) {
+                scrollTop = docEl.scrollTop;
+                scrollLeft = docEl.scrollLeft;
+            } else {
+                scrollTop = body.scrollTop;
+                scrollLeft = body.scrollLeft;
+            }
+
+            return {
+                scrollTop: scrollTop,
+                scrollLeft: scrollLeft
+            };
+        }
+
+        function getCoords(elem) {
+            // via http://stackoverflow.com/a/26230989/117014
+            var box = elem.getBoundingClientRect();
+
+            var body = document.body;
+            var docEl = document.documentElement;
+
+            var scroll = getPageScroll();
+            var scrollTop = scroll.scrollTop;
+            var scrollLeft = scroll.scrollLeft;
+
+            if (window.hasOwnProperty('pageYOffset')) {
+                scrollTop = window.pageYOffset;
+                scrollLeft = window.pageXOffset;
+            } else if (docEl.hasOwnProperty('scrollTop')) {
+                scrollTop = docEl.scrollTop;
+                scrollLeft = docEl.scrollLeft;
+            } else {
+                scrollTop = body.scrollTop;
+                scrollLeft = body.scrollLeft;
+            }
+
+            var clientTop = docEl.clientTop || body.clientTop || 0;
+            var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+            var top  = box.top +  scrollTop - clientTop;
+            var left = box.left + scrollLeft - clientLeft;
+
+            return {
+                top: Math.round(top),
+                left: Math.round(left)
+            };
+        }
+
+        function depthPercent() {
+            var eltTopPosition = getCoords(elt).top;
+            var articleHeight = elt.offsetHeight;
+            var eltBottomPosition = eltTopPosition + articleHeight;
+            var scrollTop = getPageScroll().scrollTop;
+            var scrollBottom = scrollTop + window.innerHeight;
+            var percent = (scrollBottom - eltTopPosition) / articleHeight;
+
+            return percent;
+        }
+
+        function percentBucket(n) {
+            return Math.round(n * 10) * 10;
+        }
+
+        function trackDepth() {
+            var percent = depthPercent();
+            var bucket = percentBucket(percent);
+            //console.log(bucket, previousBucket);
+            if (bucket > previousBucket) {
+                //console.log("Bucket is bigger");
+                previousBucket = bucket;
+                callback(bucket);
+            }
+            //console.log(previousBucket, "PB");
+        }
+
+        window.addEventListener('scroll', function(event) {
+            trackDepth();
+        });
+
+        // trackDepth();
+    };
 
     return lib;
 });
